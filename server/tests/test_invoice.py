d0b0930048034f9e2c30d84fa778ba189167c090
Douglas Gatimu <douglasgatimu@gmail.com>
2025-09-17 23:44:17 +0300

    postgres attempt to work with ci/cd.

--- server/tests/test_invoice.py
+++ server/tests/test_invoice.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
