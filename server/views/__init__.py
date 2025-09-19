d0e7df7ed4736df54360e43f86c9fbbffb8a752c
Douglas Gatimu <douglasgatimu@gmail.com>
2025-09-20 02:05:25 +0300

    register invoices, bookings, agreement blueprints.

--- server/views/__init__.py
+++ server/views/__init__.py
@@ -6,7 +6,9 @@ from .spaces import spaces_bp
 from .user_roles import user_roles_bp
 from .password_reset import reset_tokens_bp
 from .auth import auth_bp
-
+from .bookings import bookings_bp
+from .agreements import agreements_bp
+from .invoices import invoices_bp
 
 api_bp = Blueprint("api", __name__)
 
@@ -18,3 +20,6 @@ api_bp.register_blueprint(spaces_bp, url_prefix="/spaces")
 api_bp.register_blueprint(user_roles_bp, url_prefix="/user_roles")
 api_bp.register_blueprint(reset_tokens_bp, url_prefix="/reset_tokens")
 api_bp.register_blueprint(auth_bp, url_prefix="/auth")
+api_bp.register_blueprint(bookings_bp, url_prefix="/bookings")
+api_bp.register_blueprint(agreements_bp, url_prefix="/agreements")
+api_bp.register_blueprint(invoices_bp, url_prefix="/invoices")
