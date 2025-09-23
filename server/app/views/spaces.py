5edbd142f444da6dfc373725fa8cf4bb049a4ade
Antony Omondi <tonymboya13@gmail.com>
2025-09-23 16:43:23 +0300

    added template creation by owner

--- server/app/views/spaces.py
+++ server/app/views/spaces.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import Space, User, db, AgreementTemplate
+from app.models import Space, User, AgreementTemplate, db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 spaces_bp = Blueprint('spaces', __name__)
@@ -16,7 +16,7 @@ def create_space():
 
     if "owner" not in current_user.get_roles():
         return jsonify({'error': 'Only owners can create spaces'}), 403
-    
+
     data = request.get_json()
     try:
         space = Space(
@@ -30,7 +30,16 @@ def create_space():
             max_guests=data['max_guests'],
         )
         db.session.add(space)
+        db.session.flush()
+
+        template = AgreementTemplate(
+            owner_id=current_user_id,
+            space_id=space.id,
+            terms=data['terms']
+        )
+        db.session.add(template)
         db.session.commit()
+
         return jsonify(space.to_dict()), 201
     except Exception as e:
         db.session.rollback()
@@ -47,11 +56,11 @@ def get_spaces():
 def get_space(space_id):
     space = Space.query.get_or_404(space_id)
 
-    
+
     latest_template = (
         AgreementTemplate.query
         .filter_by(space_id=space.id)
-        .order_by(AgreementTemplate.created_at.desc()) 
+        .order_by(AgreementTemplate.created_at.desc())
         .first()
     )
 
