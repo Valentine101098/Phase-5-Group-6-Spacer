760eb3bbd1dd0b621b94ca39cf0fd45132bf5bac
Antony Omondi <tonymboya13@gmail.com>
2025-09-23 18:18:31 +0300

    added space details component

---
+++ client/src/components/SpaceDetails.jsx
@@ -0,0 +1,27 @@
+export default function SpaceDetails({ space, onClose }) {
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
+            <div className="bg-white dark:bg-gray-900 rounded-2xl shadow-lg w-3/4 p-6 max-h-[90vh] overflow-y-auto">
+                <button onClick={onClose} className="text-gray-500 float-right">More Details</button>
+                <h2 className="text-2xl font-bold mb-4">{space.title}</h2>
+
+                <div className="grid grid-cols-2 gap-2 mb-4">
+                    {space.images.slice(0, 4).map((imgUrl, index) => (
+                        <img
+                            key={index}
+                            src={imgUrl}
+                            alt={`Space Image ${index + 1}`}
+                            className="w-full h-40 object-cover rounded-lg"
+                        />
+                    ))}
+                </div>
+
+                <p className="mb-3">{space.description}</p>
+                <p><strong>Space Type:</strong> {space.space_type}</p>"
+                <p><strong>Maximum Guests:</strong> {space.max_guests}</p>
+                <p><strong>Price:</strong> Kshs {space.price_per_hour}/hr</p>
+                <p><strong>Status:</strong> {space.status}</p>
+            </div>
+        </div>
+    )
+}
