257e141c66700e629da9c8b4ff5b250c1811f5a4
Antony Omondi <tonymboya13@gmail.com>
2025-09-24 13:16:29 +0300

    Details page complete

--- client/src/components/SpaceDetails.jsx
+++ client/src/components/SpaceDetails.jsx
@@ -1,9 +1,9 @@
 export default function SpaceDetails({ space, onClose }) {
     return (
         <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
-            <div className="bg-white dark:bg-gray-900 rounded-2xl shadow-lg w-3/4 p-6 max-h-[90vh] overflow-y-auto">
-                <button onClick={onClose} className="text-gray-500 float-right">Close</button>
-                <h2 className="text-2xl font-bold mb-4">{space.title}</h2>
+            <div className="bg-white dark:bg-blue-700 rounded-2xl shadow-lg w-1\2 p-6 max-h-[90vh] overflow-y-auto">
+                <button onClick={onClose} className="text-white float-right">Close</button>
+                <h2 className="text-2xl text-white font-bold mb-4">{space.title}</h2>
 
                 <div className="grid grid-cols-2 gap-2 mb-4">
                     {space.images.slice(0, 4).map((imgUrl, index) => (
@@ -16,11 +16,11 @@ export default function SpaceDetails({ space, onClose }) {
                     ))}
                 </div>
 
-                <p className="mb-3">{space.description}</p>
-                <p><strong>Space Type:</strong> {space.space_type}</p>"
-                <p><strong>Maximum Guests:</strong> {space.max_guests}</p>
-                <p><strong>Price:</strong> Kshs {space.price_per_hour}/hr</p>
-                <p><strong>Status:</strong> {space.status}</p>
+                <p className="mb-3 text-white">{space.description}</p>
+                <p className="text-white"><strong>Space Type:</strong> {space.space_type}</p>"
+                <p className="text-white"><strong>Maximum Guests:</strong> {space.max_guests}</p>
+                <p className="text-white"><strong>Price:</strong> Kshs {space.price_per_hour}/hr</p>
+                <p className="text-white mb-2"><strong>Status:</strong> {space.status}</p>
 
                 {space.template?.terms && (
                     <div className="mt-4 border-t pt-4">
@@ -30,6 +30,15 @@ export default function SpaceDetails({ space, onClose }) {
                         </div>
                     </div>
                 )}
+                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+                    Book
+                </button>
+                <button
+                    onClick={() => setShowReviews((prev) => !prev)}
+                    className="bg-yellow-500 text-white px-4 py-2 ml-2 rounded-lg hover:bg-yellow-600"
+                >
+                    Reviews ⭐
+                </button>
             </div>
         </div>
     )
