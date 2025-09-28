b2358ee42c150bdb59bd64798a8c1c7e524988dd
Antony Omondi <tonymboya13@gmail.com>
2025-09-28 21:01:40 +0300

    Renamed the book button

--- client/src/components/SpaceCard.jsx
+++ client/src/components/SpaceCard.jsx
@@ -49,7 +49,7 @@ export default function SpaceCard({ space }) {
                     {space.status === "available" ?
                     <Link to={`/spaces/${space.id}/booking`}>
                     <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-full">
-                        Book
+                        Quick Book
                     </button> </Link> : null}
                 </div>
             </div>
